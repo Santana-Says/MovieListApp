e39ff2deb88c4260f5fb0c989b27ea520c60008b
Jeffrey Santana <santanajny@gmail.com>
2017-08-27 11:45:14 -0400

    Initial Commit

---
+++ JMDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JMDB
+//
+//  Created by Jeffrey Santana on 8/27/17.
+//  Copyright © 2017 Jeffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
